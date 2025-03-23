69351bf4a472add7eed80aa1bb0ab310a61f291b
Cameron McEvenue <mcevenc@gmail.com>
2025-03-22 20:15:48 -0400

    ref: added function override to allow config to be passed in as a callback

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,35 @@
 import type { Config } from "$lib/internal/config/schema.ts";
 
-export function defineConfig(config: Config) {
+/**
+ * Defines the configuration object for the project
+ *
+ * @param config  The configuration object or a function that returns the configuration object
+ * @example
+ *
+ * import { defineConfig } from "@smootclose/quadlet-forge";
+ *
+ * // as an object
+ * export default defineConfig({
+ *   distro: "fedora-coreos",
+ *   user: "quadlet",
+ * });
+ *
+ * // or as a function
+ * export default defineConfig(() => {
+ *
+ *  // some custom logic to generate the config
+ *
+ * return {
+ *  distro: "fedora-coreos",
+ *  user: "quadlet",
+ * }
+ * })
+ */
+export function defineConfig(config: Config): Config;
+export function defineConfig(config: () => Config): Config;
+export function defineConfig(config: Config | (() => Config)): Config {
+  if (typeof config === "function") {
+    return config();
+  }
   return config;
 }
