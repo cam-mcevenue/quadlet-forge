63891df62ca48b75b36a640c9c8bbacc27122925
Cameron McEvenue <mcevenc@gmail.com>
2025-03-23 01:01:13 -0400

    feat: added constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,10 @@
+export const DISTRO_HOME_DIR = {
+  ubuntu: "/home",
+  debian: "/home",
+  coreos: "/var/home",
+} as const;
+
+export const PODMAN_PATHS = {
+  quadlet: ".config/containers/systemd",
+  systemd: ".config/systemd/user",
+};
