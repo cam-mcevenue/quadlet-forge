986f9fab9132e0dd4e313b9ad630d3ba4f8dbe1b
Cameron McEvenue <mcevenc@gmail.com>
2025-03-22 20:17:24 -0400

    added some schema logic

--- src/internal/config/schema.ts
+++ src/internal/config/schema.ts
@@ -1,8 +1,15 @@
 import { z } from "npm:zod";
 
 const distros = z.enum(["ubuntu", "debian", "fedora-coreos"]);
+const username = z
+  .string()
+  .min(2, "Username must be at least 2 characters long")
+  .refine((val) => val === val.toLowerCase(), {
+    message: "User name's must be lowercase",
+  });
 
 export type Config = z.infer<typeof configSchema>;
 export const configSchema = z.object({
   distro: distros,
+  user: username,
 });
