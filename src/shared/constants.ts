46db0de16c794857091c8be8de634df4e2ff6b19
Cameron McEvenue <mcevenc@gmail.com>
2025-03-24 21:44:02 -0400

    chore: export types

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,9 +1,11 @@
+export type DistroHomeDir = typeof DISTRO_HOME_DIR;
 export const DISTRO_HOME_DIR = {
   ubuntu: "/home",
   debian: "/home",
   coreos: "/var/home",
 } as const;
 
+export type PodmanPathMap = typeof PODMAN_PATHS;
 export const PODMAN_PATHS = {
   quadlet: ".config/containers/systemd",
   systemd: ".config/systemd/user",
